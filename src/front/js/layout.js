a81555fad5f6e5bd3658789b6f1c7191e9ed30df
Demian Sotomayor <122990398+Demian-Sotomayor@users.noreply.github.com>
2023-12-28 19:23:20 +0000

    Movi el proyecto a otro repo, solucione problema de tabla fantasma en base de datos que daba error, arreglé problema de cors y cambié las llamadas en el flux para que funcionen con el link de backend de .env (modificar dependiendo del repositorio y dejar visibilidad pública)

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
-import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import Home from "./pages/home.jsx";
+import injectContext from "./store/appContext.js";
+import Navbar from "./component/Navbar.jsx";
+import Registro from "./pages/registro.jsx";
+import LogIn from "./pages/logIn.jsx";
+import Private from "./pages/private.jsx";
+import PrivateRoute from "./component/PrivateRoute.jsx";
 
 //create your first component
 const Layout = () => {
@@ -17,21 +15,21 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
-
     return (
         <div>
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Registro />} path="/registro" />
+                        <Route element={<LogIn />} path="/acceso" />
+                        <Route element={
+                        <PrivateRoute>
+                            <Private />
+                        </PrivateRoute>
+                        } path="/private" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
-                </ScrollToTop>
             </BrowserRouter>
         </div>
     );
